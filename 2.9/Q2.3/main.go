4eeec1d81c2b101d825b2b453168957933630566
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-07-12 22:44:55 +0900

    jsonの作成と圧縮

--- 2.9/Q2.3/main.go
+++ 2.9/Q2.3/main.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -22,12 +24,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	encoder.Encode(source)
 
 	// gzipに圧縮する
-	gz_q := gzip.NewWriter()
+	file, err := os.Create("test.json.gz")
+	if err != nil {
+		panic(err)
+	}
+	gz_w := gzip.NewWriter(file)
+	gz_w.Header.Name = "test.json"
+	io.Copy(gz_w, &buffer)
+	defer gz_w.Close()
 
 	// 圧縮に平文渡す
 	// 標準出力に平文わたす
 	// レスポンスライターに圧縮わたす
-
+	io.Copy(w, file)
 }
 
 func main() {
