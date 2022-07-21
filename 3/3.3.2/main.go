81e81602d40e10a403a2bc858baf1d11c1137b38
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-07-21 20:42:20 +0900

    関数内に記述

--- 3/3.3.2/main.go
+++ 3/3.3.2/main.go
@@ -8,22 +8,20 @@ import (
 	"strings"
 )
 
-var reader io.Reader = strings.NewReader("テストデータ")
-
-// strings.Readerはio.Closerではないが、
-// io.NopCloserでCloseメソッドを持たせることができる
-var readCloser io.ReadCloser = io.NopCloser(reader)
-
-// io.Reader とio.Writerをつなげて
-// io.ReadWriter型のオブジェクトを作ることができる
-var buf bytes.Buffer
-var r = bufio.NewReader(reader)
-var w = bufio.NewWriter(&buf)
-var readWriter io.ReadWriter = bufio.NewReadWriter(r, w)
-
 func main() {
+	var reader io.Reader = strings.NewReader("テストデータ")
+
+	// strings.Readerはio.Closerではないが、
+	// io.NopCloserでCloseメソッドを持たせることができる
+	var readCloser io.ReadCloser = io.NopCloser(reader)
 	readCloser.Close() // 何も起きない
 
+	// io.Reader とio.Writerをつなげて
+	// io.ReadWriter型のオブジェクトを作ることができる
+	var buf bytes.Buffer
+	var r = bufio.NewReader(reader)
+	var w = bufio.NewWriter(&buf)
+	var readWriter io.ReadWriter = bufio.NewReadWriter(r, w)
 	// readWriterを読んでreadWriterに書き込む
 	io.Copy(readWriter, readWriter)
 	// wに値が入っている
