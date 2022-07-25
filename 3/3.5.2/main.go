b56eed2fce3e9db17ba7f029e8cd49b25db78a54
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-07-25 19:44:27 +0900

    コメント修正

--- 3/3.5.2/main.go
+++ 3/3.5.2/main.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main() {
-	// 32ビットのビッグエンディアンのデータ(10000)
+	// 32ビットのビッグエンディアンのデータ(10000) 0x2710
 	data := []byte{0x00, 0x00, 0x27, 0x10}
 	var j int32
 	var i int32
 
+	// 0x10270000として格納される
 	binary.Read(bytes.NewReader(data), binary.LittleEndian, &j)
 	fmt.Printf("litteleEndian: %d\n", j)
 	// エンディアンの変換
