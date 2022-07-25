dd0494d63dca064fef0be07a572e8e57f0f41db8
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-07-25 18:20:44 +0900

    io.SectionReader

--- 3/3.5.1/main.go
+++ 3/3.5.1/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"os"
 	"strings"
 )
 
 func main() {
+	ioSectionReader()
+	ioLimitReader()
+}
+
+//io.SectionReader
+func ioSectionReader() {
+	// 読み取りの開始位置を指定できる。
 	reader := strings.NewReader("Example of io.SectionReader\n")
 	sectionReader := io.NewSectionReader(reader, 14, 7)
 	io.Copy(os.Stdout, sectionReader)
+
+	// 引数がio.ReaderAt
+	// io.ReaderAtを満たさないio.Readerの場合(bytes.Bufferとか)
+	buffer := bytes.NewBuffer([]byte("io.ReadAtを満たさないbytes.Bufferを使用する場合"))
+	// 文字列やバイト列に書き出し、strings.Readerやbytes.Readerでラップしてから渡す
+	data, _ := io.ReadAll(buffer)
+	byteRecader := bytes.NewReader(data)
+	sr := io.NewSectionReader(byteRecader, 14, 7)
+
+	io.Copy(os.Stdout, sr)
+
 }
 
 //io.LimitReader
