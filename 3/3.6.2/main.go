a921f0733784828dd578ffe16c03c0783c476e6b
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-07-31 17:22:00 +0900

    カンマスペースで区切る

--- 3/3.6.2/main.go
+++ 3/3.6.2/main.go
@@ -5,13 +5,15 @@ import (
 	"strings"
 )
 
-var source = "123 1.234 1.0e4 test"
+// var source = "123 1.234 1.0e4 test"
+var source = "123, 1.234, 1.0e4, test"
 
 func main() {
 	reader := strings.NewReader(source)
 	var i int
 	var f, g float64
 	var s string
-	fmt.Fscan(reader, &i, &f, &g, &s)
+	// fmt.Fscan(reader, &i, &f, &g, &s)
+	fmt.Fscanf(reader, "%v, %v, %v, %v", &i, &f, &g, &s)
 	fmt.Printf("i=%#v f=%#v g=%#v s=%#v\n", i, f, g, s)
 }
