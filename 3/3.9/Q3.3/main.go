e23722c4a2ebc74b8a3b12d05c786eafb46a7caa
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-08-05 17:05:10 +0700

    zip

---
+++ 3/3.9/Q3.3/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	// sr := strings.NewReader("sample text for zip")
+
+	file, err := os.Create("sample.zip")
+	// file, err := os.OpenFile("sample.txt", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+	zipWriter := zip.NewWriter(file)
+	defer zipWriter.Close()
+
+	newfile, err := zipWriter.Create("newfile.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	io.Copy(newfile, strings.NewReader("zip test"))
+}
