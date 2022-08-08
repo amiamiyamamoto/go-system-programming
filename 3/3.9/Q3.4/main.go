b24a17da53c1d2ea184585e7bac9de035d56c09f
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-08-08 22:28:56 +0700

    関数化

--- 3/3.9/Q3.4/main.go
+++ 3/3.9/Q3.4/main.go
@@ -19,33 +19,33 @@ func handeler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/zip")
 	w.Header().Set("Content-Disposition", "attachment; filename=ascii_sample.zip")
 
-	// file := makezipfile("sample.zip")
+	file := makezipfile("sample.zip")
 	// defer file.Close()
 	// iti, _ := file.Seek(0, 1)
 
 	//ここから
 
-	file, err := os.Create("sample.zip")
+	// file, err := os.Create("sample.zip")
 
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
 
-	zipWriter := zip.NewWriter(file)
-	// defer zipWriter.Close()
+	// zipWriter := zip.NewWriter(file)
+	// // defer zipWriter.Close()
 
-	watxt, err := zipWriter.Create("a.text")
-	if err != nil {
-		panic(err)
-	}
-	io.Copy(watxt, strings.NewReader("zip web download"))
-	// iti, _ := file.Seek(0, 1)
-	// fmt.Println("makezipfile: ", iti)
-	// iti2, _ := file.Seek(0, 0)
-	zipWriter.Close()
+	// watxt, err := zipWriter.Create("a.text")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// io.Copy(watxt, strings.NewReader("zip web download"))
+	// // iti, _ := file.Seek(0, 1)
+	// // fmt.Println("makezipfile: ", iti)
+	// // iti2, _ := file.Seek(0, 0)
+	// zipWriter.Close()
 
-	file.Seek(0, 0)
+	// file.Seek(0, 0)
 	// fmt.Println("makezipfile: ", iti2)
 
 	//ここまで
@@ -61,18 +61,19 @@ func makezipfile(filename string) *os.File {
 	if err != nil {
 		panic(err)
 	}
+
 	// file.Close()
 
 	zipWriter := zip.NewWriter(file)
-	defer zipWriter.Close()
 
 	w, err := zipWriter.Create("a.text")
 	if err != nil {
 		panic(err)
 	}
 	io.Copy(w, strings.NewReader("zip web download"))
-	iti, _ := file.Seek(0, 1)
-	fmt.Println("makezipfile: ", iti)
+	zipWriter.Close()
+	// iti, _ := file.Seek(0, 1)
+	// fmt.Println("makezipfile: ", iti)
 	iti2, _ := file.Seek(0, 0)
 	fmt.Println("makezipfile: ", iti2)
 	return file
