c62ea0e969a80561f066f52edff796658729b934
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-08-08 22:03:39 +0700

    zipファイル作成の関数化をしないで動くところまで

--- 3/3.9/Q3.4/main.go
+++ 3/3.9/Q3.4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -18,16 +19,49 @@ func handeler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/zip")
 	w.Header().Set("Content-Disposition", "attachment; filename=ascii_sample.zip")
 
-	makezipfile("sample.zip")
+	// file := makezipfile("sample.zip")
+	// defer file.Close()
+	// iti, _ := file.Seek(0, 1)
+
+	//ここから
+
+	file, err := os.Create("sample.zip")
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	zipWriter := zip.NewWriter(file)
+	// defer zipWriter.Close()
+
+	watxt, err := zipWriter.Create("a.text")
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(watxt, strings.NewReader("zip web download"))
+	// iti, _ := file.Seek(0, 1)
+	// fmt.Println("makezipfile: ", iti)
+	// iti2, _ := file.Seek(0, 0)
+	zipWriter.Close()
+
+	file.Seek(0, 0)
+	// fmt.Println("makezipfile: ", iti2)
+
+	//ここまで
+	// fmt.Println("main:", iti)
+
+	io.Copy(w, file)
 
 }
 
 // zipファイルを作成する
-func makezipfile(filename string) {
+func makezipfile(filename string) *os.File {
 	file, err := os.Create(filename)
 	if err != nil {
 		panic(err)
 	}
+	// file.Close()
 
 	zipWriter := zip.NewWriter(file)
 	defer zipWriter.Close()
@@ -37,5 +71,10 @@ func makezipfile(filename string) {
 		panic(err)
 	}
 	io.Copy(w, strings.NewReader("zip web download"))
+	iti, _ := file.Seek(0, 1)
+	fmt.Println("makezipfile: ", iti)
+	iti2, _ := file.Seek(0, 0)
+	fmt.Println("makezipfile: ", iti2)
+	return file
 
 }
