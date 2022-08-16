66d2ca038a02f041e25d890fbf75a01e6ea2609c
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-08-16 13:31:40 +0700

    チャネルとselect文

--- 4/4.2.4/main.go
+++ 4/4.2.4/main.go
@@ -1,14 +1,33 @@
 package main
 
-func main() {
-	//
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	reader := make(chan string, 5)
+	exit := make(chan bool, 1)
+
+	go func() {
+		reader <- "test1"
+		reader <- "test2"
+		reader <- "test3"
+	}()
+	go func() {
+		time.Sleep(1 * time.Second)
+		exit <- true
+	}()
+
+L:
 	for {
 		select {
-		case data := <-reader
+		case data := <-reader:
+			fmt.Println(data)
 		case <-exit:
 			// ループを抜ける
-			break
+			break L
 		}
 	}
+
 }
