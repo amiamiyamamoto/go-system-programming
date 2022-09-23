bdf459eb0684d92d1660affed9a03ecb7e5cf06d
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-09-23 23:56:24 +0900

    ルールに一致するファイル名の一覧を取得する

--- 9/9.5.6/main.go
+++ 9/9.5.6/main.go
@@ -7,5 +7,13 @@ import (
 
 // ファイル名のパターンにマッチするファイルの抽出
 func main() {
+	// パターンと指定したファイル名が同じかどうか調べる
 	fmt.Println(filepath.Match("image-*.png", "image-100.png"))
+
+	//ルールに合致するファイル名の一覧を取得する
+	files, err := filepath.Glob("./*.png")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(files)
 }
