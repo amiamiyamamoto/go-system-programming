9bc64fcdbfbee68f077cc22faa4c504552cf35d8
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-09-27 11:19:26 +0900

    Windowsでのファイルロック

---
+++ 10/10.2.2/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"sync"
+	"syscall"
+)
+
+// Windowsでのファイルロック
+var (
+	modkernel32      = syscall.NewLazyDLL("kernel32.dll")
+	procLockFileEx   = modkernel32.NewProc("LockFileEx")
+	procUnlockFileEx = modkernel32.NewProc("UnlockFileEx")
+)
+
+type FileLock struct {
+	m  sync.Mutex
+	fd syscall.Handle
+}
+
+func NewFileLock(filename string) *FileLock {
+	if filename == "" {
+		panic("filename needed")
+	}
+	fd, err := syscall.CreateFile(
+		&(syscall.StringToUTF16(filename)[0]),
+		syscall.GENERIC_READ|syscall.NENERIC_WRITE,
+		syscall.FILE_SHARE_READ|syscall.FILE_SHARE_WRITE,
+		nil,
+		syscall.OPEN_ALWAYS,
+		syscall.FILE_ATTRIBUTE_NORMAL,
+		0)
+	if err != nil {
+		panic(err)
+	}
+	return &FileLock{fd: fd}
+}
+
+func (m *FileLock) Unlock() {
+	var ol syscall.Overlapped
+	r1, _, e1 := syscall.Syscall6(
+		procUnlockFileEx.Addr(),
+		5
+		uintptr(m.fd),
+		uintptr(LOCKFILE_EXCLUSEIVE_LOCK),
+		uintptr(0),
+		uintptr(1),
+		uintptr(0),
+		uintptr(unsafe.Pointer(ol)),
+		0)
+		if r1 == 0 {
+			if e1 != 0 {
+				panic(error(e1))
+			} else {
+				panic(syscall.EINVAL)
+			}
+		}
+		m.m.Unlock()
+}
