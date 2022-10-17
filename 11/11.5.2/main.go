e279c97a6b23d133730ddb54cf4665805c53dc6d
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-10-17 15:20:44 +0900

    入力されたテキストの分解

---
+++ 11/11.5.2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/google/shlex"
+)
+
+func parseCmd(cmdStr string) (cmd string, args []string, err error) {
+	l := shlex.NewLexer(strings.NewReader(cmdStr))
+	cmd, err = l.Next()
+	if err != nil {
+		return
+	}
+	for ; token, err := l.Next(); err != nil {
+		args = append(args, token)
+	}
+	return
+}
+
+// 入力されたテキストの分解はスペースで区切られた文字列？
+func main() {
+	r := regexp.MustCompile(`\s+`)
+	tokens := r.Split(`echo hello`, -1)
+	for i, t := range tokens {
+		fmt.Println(i, t)
+	}
+}
