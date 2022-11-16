79cb7b114cf1146bdf0795eb9dc0b762f13f6f66
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-11-16 10:45:19 +0900

    sync.Mutex/sync.RWMutex

---
+++ 14/14.7.1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "sync"
+
+var id int
+
+func generateId(mutex *sync.Mutex) int {
+	// Lock()/Unlock()をペアで呼び出してロックする
+	mutex.Lock()
+	id++
+	result := id
+	mutex.Unlock()
+	return result
+}
+func main() {
+	//sync.Mutex構造体の変数宣言
+	// 次の宣言をしてもポインタ型になるだけで正常に動作する
+	// mutex := new(sync.Mutex)
+	var mutex sync.Mutex
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			fmt.Pirntf("id: %d\n", generateId(&mutex))
+		}()
+	}
+
+}
