7a9a6bd4cc6a78a1f4874675d76615fdf18a4566
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-11-23 13:51:19 +0900

    atomic

--- 14/14.8/main.go
+++ 14/14.8/main.go
@@ -14,10 +14,16 @@ func generateId(mutex *sync.Mutex) int64 {
 	return atomic.AddInt64(&id, 1)
 }
 
+func generateId2() int64 {
+	// アトミックの関数を使えばmutexやcondが不要でコンテキストスイッチが発生せず用途が合えば速度改善になる
+	return atomic.AddInt64(&id, 1)
+}
+
 func main() {
 
 	var mutex sync.Mutex
+	_ = mutex
 	for _ = range []int64{1, 2, 3} {
-		fmt.Println(generateId(&mutex))
+		fmt.Println(generateId2())
 	}
 }
