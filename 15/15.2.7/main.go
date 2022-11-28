f7f090487dac4deb5338f4e70def9aab36713c69
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-11-28 14:25:32 -0800

    ファイル追加

---
+++ 15/15.2.7/main.go
@@ -0,0 +1,11 @@
+package main
+
+// 計算：元金均等
+func calc(id, price int, interestRate float64, year int) {
+	months := year * 12
+	interest := 0
+	for i := 0; i < months; i++ {
+		balance := price * (months - i) / months
+		interest += int(float64(balance) * interestRate / 12)
+	}
+}
