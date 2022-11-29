1e2f3af1a4a8ae0378aa2f63400533333bab09c8
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-11-29 13:55:51 -0800

    決まった数のgoroutineでタスクを消化

--- 15/15.2.7/main.go
+++ 15/15.2.7/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
 // 計算：元金均等
 func calc(id, price int, interestRate float64, year int) {
 	months := year * 12
@@ -8,4 +14,34 @@ func calc(id, price int, interestRate float64, year int) {
 		balance := price * (months - i) / months
 		interest += int(float64(balance) * interestRate / 12)
 	}
+	fmt.Printf("year=%d total=%d interest=%d id=%d\n", year, price+interest, interest, id)
+}
+
+func worker(id, price int, interestRate float64, years chan int, wg *sync.WaitGroup) {
+	// タスクが無くなってタスクのチャネルがcloseされるまで無限ループ
+	for year := range years {
+		calc(id, price, interestRate, year)
+		wg.Done()
+	}
+}
+
+func main() {
+	// 借入額
+	price := 40000000
+	// 利子　1.1%固定
+	interestRate := 0.011
+	// タスクはchanに格納
+	years := make(chan int, 35)
+	for i := 1; i < 36; i++ {
+		years <- i
+	}
+	var wg sync.WaitGroup
+	wg.Add(35)
+	// CPUコア数分のgoroutine起動
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go worker(i, price, interestRate, years, &wg)
+	}
+	// 全てのワーカーが終了する
+	close(years)
+	wg.Wait()
 }
