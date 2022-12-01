57d505657766f4758ac28c464a95391651d7e9ec
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-12-01 14:01:06 -0800

    依存関係のあるタスクを表現する

--- 15/15.2.8/main.go
+++ 15/15.2.8/main.go
@@ -1,7 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func readFile(path string) chan string {
 	promise := make(chan string)
-	promise <- path
+	go func() {
+		content, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Printf("read error %s\n", err.Error())
+		} else {
+			// 約束を果たした
+			promise <- string(content)
+		}
+	}()
 	return promise
+}
+
+func printFunc(futureSource chan string) chan []string {
+	// 文字列中の関数一覧を返す Futureを返す
+	promise := make(chan []string)
+	go func() {
+		var result []string
+		// fetureが解決するまで待って実行
+		for _, line := range strings.Split(<-futureSource, "\n") {
+			if strings.HasPrefix(line, "func ") {
+				result = append(result, line)
+			}
+		}
+		// 約束を果たした
+		promise <- result
+	}()
+	return promise
+}
+
+func main() {
+	futureSource := readFile("future?promise.go")
+	futureFuncs := printFunc(futureSource)
+	fmt.Println(strings.Join(<-futureFuncs, "\n"))
 }
