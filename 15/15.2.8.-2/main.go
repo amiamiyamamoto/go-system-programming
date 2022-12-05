b31560e6f3b8a1d103620931213a3503f04fa222
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-12-05 19:38:00 +0900

    返り値追加

--- 15/15.2.8.-2/main.go
+++ 15/15.2.8.-2/main.go
@@ -1,3 +1,5 @@
+package main
+
 type StringFuture struct {
 	receiver chan string
 	cache    string
@@ -5,19 +7,20 @@ type StringFuture struct {
 
 func NewStringFuture() (*StringFuture, chan string) {
 	f := &StringFuture{
-		receiver: make(chan string)
+		receiver: make(chan string),
 	}
 	return f, f.receiver
 }
 
 func (f *StringFuture) Get() string {
-	r, ok := <- f.receiver
+	r, ok := <-f.receiver
 	if ok {
 		close(f.receiver)
 		f.cache = r
 	}
+	return f.cache
 }
 
-func (f * StringFuture) close() {
+func (f *StringFuture) Close() {
 	close(f.receiver)
 }
