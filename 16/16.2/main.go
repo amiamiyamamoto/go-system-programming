581772967152f6ba0f561be54143274a99df9ed2
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-12-16 17:52:09 +0900

    配列の範囲外アクセスのエラー

--- 16/16.2/main.go
+++ 16/16.2/main.go
@@ -17,4 +17,20 @@ func main() {
 
 	fmt.Println(a, b, c, d)
 
+	// 格納するデータの数が決まっている場合に配列を使用する
+	// 固定長配列は要素数まで含めて型
+	// 3要素の浮動小数点を座標を扱う構造体とする
+	type Vector3D [3]float64
+
+	// R,G,B,Aのそれぞれが0-255の範囲で表現できる色構造体
+	type Color [4]uint8
+
+	// 配列の範囲外アクセスもエラーになる
+	// fmt.Println(c[10])
+	// fmt.Println(c[-1])
+
+	// 変数を使用してアクセスすると実行時にエラーになる
+	num := 10
+	fmt.Println(c[num])
+	// panic: runtime error: index out of range [10] with length 4
 }
