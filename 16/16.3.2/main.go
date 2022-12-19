335247da1840382b100b1aab356b79b1cfc3f8b7
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-12-19 13:45:46 +0900

    スライスのメモリ確保とパフォーマンス改善のヒント

--- 16/16.3.2/main.go
+++ 16/16.3.2/main.go
@@ -6,4 +6,13 @@ func main() {
 	// 長さ1、確保された要素2のスライスを作成
 	s := make([]int, 1, 2)
 	fmt.Println(&s[0], len(s), cap(s))
+
+	// 1要素追加（確保された範囲内）
+	s = append(s, 1)
+	fmt.Println(&s[0], len(s), cap(s))
+
+	// さらに要素を追加（新しく配列が確保され直す）
+	s = append(s, 2)
+	fmt.Println(&s[0], len(s), cap(s))
+	// ↑スライスの戦闘要素のアドレスも変わる！
 }
