33a9e07bf3133592c1181b7dab43e9b1ace24705
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-12-20 16:07:38 +0900

    sync.Poolによるアロケート回数の削減

---
+++ 16/16.3.4/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// Poolを作成。Newで新規作成時のコードを実装
+	var count int
+	pool := sync.Pool{
+		New: func() interface{} {
+			count++
+			return fmt.Sprintf("created: %d", count)
+		},
+	}
+	// 追加した要素から受け取れる
+	// プールが空だと新規作成
+	pool.Put("manualya added: 1")
+	pool.Put("manualya added: 2")
+	fmt.Println(pool.Get())
+	fmt.Println(pool.Get())
+	fmt.Println(pool.Get()) // これは新規作成
+}
