0dedba8cbf349cf6489622b6886ff5838a27bd81
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-12-23 16:28:09 +0900

    アセンブリ実行コードにコメントを追記

--- 17/17.1/hello.s
+++ 17/17.1/hello.s
@@ -1,16 +1,16 @@
-    .global _start
+    .global _start                      # このラベルが外部公開されるという宣言
 
-    .text
+    .text                               # これから先はプログラムが始まる宣言
 _start:
-    mov     $1, %rax
-    mov     $1, %rdi
-    mov     message(%rip), %rsi
-    mov     $13, %rdx
-    syscall
+    mov     $1, %rax                    # writeシステムコール（1）
+    mov     $1, %rdi                    # 出力先ファイルハンドル（1: stdout）をセット
+    mov     message(%rip), %rsi         # 出力したいメッセージアドレスをセット
+    mov     $13, %rdx                   # 文字数をセット
+    syscall                             # writeの実行
 
-    mov     $60, %rax
-    xor     %rdi, %rdi
-    syscall
+    mov     $60, %rax                   # exitシステムコール
+    xor     %rdi, %rdi                  # リターンコード（ここではエラーなし、0）
+    syscall                             # 終了呼び出し
 
-message:
-    .ascii  "Hello, world\n"
+message:                                # データ領域のラベル（いわゆる定数名）
+    .ascii  "Hello, world\n"            # テキストデータ
