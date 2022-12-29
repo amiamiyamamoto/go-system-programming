3f9a9e66fc8f6f0e10b44aa77461ed7903aa1844
amiamiyamamoto <amiyamamoto11@gmail.com>
2022-12-29 16:57:36 +0900

    時刻フォーマットに関するコメント追加

--- 18/18.5/main.go
+++ 18/18.5/main.go
@@ -10,4 +10,13 @@ func main() {
 	fmt.Println(now.Format(time.RFC822))
 
 	fmt.Println(now.Format("2006/01/02 03:04:05 MST"))
+	// 月			1
+	// 日			2
+	// 時			3
+	// 分			4
+	// 秒			5
+	// 年			6
+	// UTCとの時差   Z07
+	// タイムゾーン	  MST
+	// 午前/午後	 PM
 }
