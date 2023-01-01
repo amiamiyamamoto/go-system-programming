eedb400cd52493998bcdf5a5f0a794e0d510975a
amiamiyamamoto <amiyamamoto11@gmail.com>
2023-01-01 23:45:03 +0900

    乱数の使い方

---
+++ A/A.1.3/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	a := make([]byte, 20)
+	rand.Read(a) //セキュリティの目的でrandを使ってはいけない
+	fmt.Println(a)
+	fmt.Println(hex.EncodeToString(a))
+	//セキュリティ目的の場合はcrypto/randを使う
+}
